f84a8f8e45c9a934c0c5b56302c92db8a4ab8fd7
codeordie <blagovestkostov@abv.bg>
2015-04-28 10:04:57 +0300

    Update matrix.cpp
    have to use code convention

--- matrix.cpp
+++ matrix.cpp
@@ -1,145 +1,296 @@
+/**
+ *  @file matrix.c
+ *  @brief the description will be used in lists and at the start of the documentation page
+ *  @author Blagovest Kostov
+ *  @copy This software is distributed under proprietary license
+ *                 #### all rights reserved
+ *  Copying and distribution of this file, with or without modification, 
+ *  are permitted in any medium provided you do not contact me about the file 
+ *  or any problems you are having with the file.
+ */
+
 #include<iostream>
 #include<cstdlib>
 #include<iomanip>
+using namespace std;
 
-using namespace std; /* sexualy transmited desease */
 
-inline void die(char*e){
-	cout<<e<<endl;
-	exit(1);
+/**
+ *  @brief Brief
+ *  
+ *  @param [in] e Parameter_Description
+ *  @return Return_Description
+ *  
+ *  @details Details
+ */
+inline void doAbort(const char*acError){
+    cout << acError << endl;
+    exit(1);
 }
 
-class mat{
- private:
-	int w,h;
-	int**a;
- public:
-
-	mat(int w,int h):w(w),h(h){
-		a=new int*[h];
-		for(int i=0;i<h;i++)
-			a[i]=new int[w];
-	}
-
-	~mat(){
-		delete[]a;
-	}
-
-	void fill(){
-		for(int i=0;i<h;i++)
-			for(int j=0;j<w;j++)
-				a[i][j]=rand()%100;
-	}
-
-	void init(){
-		for(int i=0;i<h;i++)
-			for(int j=0;j<w;j++)
-				a[i][j]=0;
-	}
-
-	void print(){
-		for(int i=0;i<h;i++){
-			for(int j=0;j<w;j++)
-					cout<<setw(8)<<a[i][j];
-			cout<<endl;
-		}
-		cout<<endl;
-	}
-
-	void input(){
-		for(int i=0;i<h;i++)
-			for(int j=0;j<w;j++){
-				cout<<"pls enter value for row "<<i<<" column "<<j<<":";
-				cin>>a[i][j];
-			}
-	}
-
-	int getElement(int m,int n){
-		return a[m][n];
-	}
-
-	void setElement(int m,int n,int value){
-		a[m][n]=value;
-	}
-
-	int getWidth(){
-		return w;
-	}
-
-	int getHeight(){
-		return h;
-	}
-
-	mat*clone(){
-		return new mat(*this);
-	}
-
-	mat*transpose(){
-		mat*tt=new mat(h,w);
-		for(int i=0;i<h;i++)
-			for(int j=0;j<w;j++)
-				tt->setElement(j,i,a[i][j]);
-		return tt;
-	}
-
-	void shiftDown(){
-		a[w]=a[0];
-		for(int i=0;i<w-1;i++)
-			a[i]++;
-	}
-
-  mat*mult(mat*b){
-      if(h!=b->w)die("Illegal matrix dimensions");
-      mat*c=new mat(w,b->h);
-      for(int i=0;i<w;i++)
-          for(int j=0;j< c->h;j++)
-              for(int k=0;k<h;k++)
-                  c->a[i][j]+=(a[i][k]*b->a[k][j]);
-      return c;
-  }
-
-	mat*rmRowCol(int r,int c){
-		mat*tt=new mat(w-1,w-1);
-		tt->init();
-		for(int i=0,k=0;i<w;i++){
-			if(i==r)
-				continue;
-			for(int j=0,l=0;j<w;j++){
-				if(j==c)
-					continue;
-				tt->setElement(k,l,a[i][j]);
-				l++;
-			}
-			k++;
-		}
-		return tt;
-	}
-
-	int det(){
-		if(w!=h)
-			die("determinant of nonsquare matrix");
-		if(w==1)
-			return a[0][0];
-		if(w==2)
-			return a[0][0]*a[1][1]-a[0][1]*a[1][0];
-		if(w==3)
-		  return a[0][0]*a[1][1]*a[2][2]+a[0][1]*a[1][2]*a[2][0]+a[0][2]*a[1][0]*a[2][1]-a[0][0]*a[1][2]*a[2][1]-a[0][1]*a[1][0]*a[2][2]-a[0][2]*a[1][1]*a[2][0];
-		else{
-			int result=0;
-			for(int i=0,sign=1;i<w;i++){
-				result+=sign*a[0][i]*rmRowCol(0,i)->det();
-				sign*=-1;
-			}
-			return result;
-		}
-	}
+class Matrix{
+  private:
+    int iWidth, iHeight;
+    int**myArray;
+  public:
+    /**
+     *  @brief Brief
+     *  
+     *  @param [in] iWidth Parameter_Description
+     *  @param [in] iHeight Parameter_Description
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    Matrix(int iWidth, int iHeight): iWidth(iWidth), iHeight(iHeight) {
+        myArray = new int*[iHeight];
+        for(int i=0;  i < iHeight;  i++)
+            myArray[i] = new int[iWidth];
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    ~Matrix(){
+        delete[] myArray;
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    void doFill(){
+        for(int i=0;i<iHeight;i++)
+            for(int j=0;j<iWidth;j++)
+                myArray[i][j] = rand()%100;
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    void doInit(){
+        for(int i = 0;  i < iHeight;  i++)
+            for(int j = 0;  j < iWidth;  j++)
+                myArray[i][j] = 0;
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    void printOut(){
+        for(int i = 0;  i < iHeight;  i++){
+            for(int j = 0;  j < iWidth;  j++)
+                cout << setw(8) << myArray[i][j];
+            cout << endl;
+        }
+        cout << endl;
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    void getInput(){
+        for(int i = 0;  i < iHeight;  i++)
+            for(int j = 0;  j < iWidth;  j++){
+                cout<<"Please enter a value for row "<< i <<" column "<< j <<":";
+                cin >> myArray[i][j];
+            }
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @param [in] m Parameter_Description
+     *  @param [in] n Parameter_Description
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    inline int getElement(int m, int n){
+        return myArray[m][n];
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @param [in] m Parameter_Description
+     *  @param [in] n Parameter_Description
+     *  @param [in] value Parameter_Description
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    inline void setElement(int m, int n, int myValue){
+        myArray[m][n] = myValue;
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    inline int getWidth(){
+        return iWidth;
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    inline int getHeight(){
+        return iHeight;
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    Matrix*doClone(){
+        return new Matrix(*this);
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    Matrix*doTranspose(){
+        Matrix*myTrans = new Matrix(iHeight,iWidth);
+        for(int i=0; i<iHeight; i++)
+            for(int j=0; j<iWidth; j++)
+                myTrans->setElement(j,i,myArray[i][j]);
+        return myTrans;
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    void shiftDown(){
+        myArray[iWidth]=myArray[0];
+        for(int i=0; i < iWidth-1; i++)
+            myArray[i]++;
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @param [in] poMatrix Parameter_Description
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    Matrix*doMultiply(Matrix*poMatrix){
+        if(iHeight!=poMatrix->iWidth)
+            doAbort("Illegal matrix dimensions");
+        Matrix*resMatrix=new Matrix(iWidth,poMatrix->iHeight);
+        for(int i=0;i<iWidth;i++)
+            for(int j = 0;  j< resMatrix->iHeight;  j++)
+                for(int k = 0;  k < iHeight;  k++)
+                    resMatrix->myArray[i][j] += (myArray[i][k]*poMatrix->myArray[k][j]);
+        return resMatrix;
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @param [in] iRow Parameter_Description
+     *  @param [in] iCol Parameter_Description
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    Matrix*rmRowCol(int iRow, int iCol){
+        Matrix*myTrans=new Matrix(iWidth-1, iWidth-1);
+        myTrans->doInit();
+        for(int i=0, k=0;  i < iWidth;  i++){
+            if(i==iRow)
+                continue;
+            for(int j=0, l=0;  j < iWidth;  j++){
+                if(j==iCol)
+                    continue;
+                myTrans->setElement(k,l,myArray[i][j]);
+                l++;
+            }
+            k++;
+        }
+        return myTrans;
+    }
+    /**
+     *  @brief Brief
+     *  
+     *  @return Return_Description
+     *  
+     *  @details Details
+     */
+    int computeDeterminant(){
+        if(iWidth!=iHeight)
+            doAbort("determinant of nonsquare matrix");
+        if(iWidth==1)
+            return myArray[0][0];
+        if(iWidth==2)
+            return myArray[0][0] * myArray[1][1]-
+                   myArray[0][1] * myArray[1][0];
+        if(iWidth==3)
+            return myArray[0][0] * myArray[1][1] * myArray[2][2]+
+                   myArray[0][1] * myArray[1][2] * myArray[2][0]+
+                   myArray[0][2] * myArray[1][0] * myArray[2][1]-
+                   myArray[0][0] * myArray[1][2] * myArray[2][1]-
+                   myArray[0][1] * myArray[1][0] * myArray[2][2]-
+                   myArray[0][2] * myArray[1][1] * myArray[2][0];
+        else{
+            int myResult=0;
+            for(int i=0,mySign=1;i<iWidth;i++){
+                myResult+=mySign*myArray[0][i]*rmRowCol(0,i)->computeDeterminant();
+                mySign  *=-1;
+            }
+            return myResult;
+        }
+    }
 
 };
-
+/**
+ *  @brief Brief
+ *  
+ *  @param [in] argc Parameter_Description
+ *  @param [in] argv Parameter_Description
+ *  @return Return_Description
+ *  
+ *  @details Details
+ */
 int main(int argc, char**argv){
-	mat*m=new mat(4,4);
-	m->input();
-	m->print();
-	cout<<"determinantata e "<<m->det();
-	return 0;
+    Matrix*myMatrix = new Matrix(4, 4);
+    myMatrix->getInput();
+    myMatrix->printOut();
+    cout<<"The determinant is: " << myMatrix->computeDeterminant() << endl;
+    return 0;
 }
+  
+/**************************************************************************************************\
+*                                                                                                  *
+*      $Log$                                                                                       *
+*                                                                                                  *
+*                                                                                                  *
+*                                                                                                  *
+*                                                                                                  *
+*                                                                                                  *
+\**************************************************************************************************/
+
